37fa5d74c4ba21d37b498ca8d7b85a7d1d4c8d34
Val Packett <val@packett.cool>
2017-04-22 17:05:55 +0300

    release 1.0

--- popup.js
+++ popup.js
@@ -3,9 +3,11 @@
 const torrentsPane = document.getElementById('torrents-pane')
 const configPane = document.getElementById('config-pane')
 
-document.getElementById('config-opener').addEventListener('click', e => {
-	browser.runtime.openOptionsPage()
-})
+for (const opener of document.querySelectorAll('.config-opener')) {
+	opener.addEventListener('click', e => {
+		browser.runtime.openOptionsPage()
+	})
+}
 
 function showConfig (server) {
 	torrentsPane.hidden = true
@@ -14,25 +16,37 @@ function showConfig (server) {
 
 const torrentsList = document.getElementById('torrents-list')
 const torrentsTpl = document.getElementById('torrents-tpl')
+const torrentsError = document.getElementById('torrents-error')
 const getArgs = {
-	ids: 'recently-active',
-	fields: ['name']
+	fields: ['name', 'percentDone', 'rateDownload', 'rateUpload']
+}
+
+function refreshTorrents (server) {
+	rpcCall('torrent-get', getArgs).then(response => {
+		const args = response.arguments
+		torrentsList.innerHTML = ''
+		for (const torr of args.torrents) {
+			const node = document.importNode(torrentsTpl.content, true)
+			node.querySelector('.torrent-name').innerHTML = torr.name
+			node.querySelector('.torrent-speeds').innerHTML =
+				'↓ ' + formatSpeed(torr.rateDownload) + 'B/s ↑ ' + formatSpeed(torr.rateUpload) + 'B/s'
+			node.querySelector('.torrent-progress').value = torr.percentDone * 100
+			torrentsList.appendChild(node)
+		}
+	}).catch(err => {
+		console.error(err)
+		torrentsError.innerHTML = 'Error: ' + err.description
+	})
 }
 
 function showTorrents (server) {
 	torrentsPane.hidden = false
 	configPane.hidden = true
-	rpcCall('torrent-get', getArgs).then(server => {
-		console.log(server)
-		const args = server.arguments
-		torrentsList.innerHTML = ''
-		for (const torr of args.torrents) {
-			console.log(torr)
-			const node = document.importNode(torrentsTpl.content, true)
-			node.querySelector('.torrent-name').innerHTML = torr.name
-			torrentsList.appendChild(node)
-		}
-	})
+	for (const opener of document.querySelectorAll('.webui-opener')) {
+		opener.href = server.base_url + 'web/'
+	}
+	refreshTorrents(server)
+	setInterval(() => refreshTorrents(server), 2000)
 }
 
 browser.storage.local.get('server').then(({server}) => {
