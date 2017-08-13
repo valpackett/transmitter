c3f9797cbb28eaa4e4c19e0c581637d366b7f407
Greg V <greg@unrelenting.technology>
2017-08-13 22:23:32 +0300

    release 1.0.2

--- options.js
+++ options.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', e => {
 	}, {})
 	delete data['']
 	/*
-	When this works everywhere, replace <all_urls> with http://transmitter.-web-extension/* in manifest.json.
+	When this works everywhere, replace <all_urls> with http://transmitter.web-extension/* in manifest.json.
 	Unfortunately, right now this is unsupported in Firefox and Edge…
 	and even worse, in Opera the API exists but requesting a user-specified host is not possible!
 
