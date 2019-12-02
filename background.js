f537c86d19a1018af94df6a95909cec5613e3e4a
Abel Keszei <keszeiabel@gmail.com>
2019-12-02 20:21:47 +0100

    implement download location chooser

--- background.js
+++ background.js
@@ -40,11 +40,15 @@ function blobToBase64 (blob) {
 	})
 }
 
-function addUrl (torrentUrl) {
-	let p
+function addUrl (torrentUrl, downloadDir) {
+	let p, params = {}
+	if (downloadDir) {
+		params = { 'download-dir': downloadDir }
+	}
 	if (torrentUrl.startsWith('magnet:')) {
 		console.log('Adding magnet', torrentUrl)
-		p = rpcCall('torrent-add', { filename: torrentUrl })
+		params.filename = torrentUrl
+		p = rpcCall('torrent-add', params)
 	} else {
 		// Download the torrent file *in the browser* to support private torrents
 		console.log('Downloading torrent', torrentUrl)
@@ -57,7 +61,8 @@ function addUrl (torrentUrl) {
 			}
 			throw new Error('Could not download torrent')
 		}).then(blobToBase64).then(b64 => {
-			return rpcCall('torrent-add', { metainfo: b64 })
+			params.metainfo = b64
+			return rpcCall('torrent-add', params)
 		})
 	}
 	return p.then(x => {
@@ -83,15 +88,43 @@ browser.webRequest.onBeforeRequest.addListener(
 
 ////// Context menu
 
-browser.contextMenus.create({
-	id: 'transmitter-add',
-	title: 'Download with Transmission remote',
-	contexts: ['link']
-})
+function createContextMenu () {
+	browser.storage.local.get('server').then(({server}) => {
+		browser.contextMenus.removeAll()
+		if (!server || !server.locations || !server.locations.length) {
+			browser.contextMenus.create({
+				id: 'transmitter-add',
+				title: 'Download with Transmission remote',
+				contexts: ['link']
+			})
+		} else {
+			browser.contextMenus.create({
+				id: 'transmitter-add',
+				title: 'Download to Default location',
+				contexts: ['link']
+			})
+			server.locations.forEach(location => {
+				browser.contextMenus.create({
+					id: 'transmitter-add-loc-' + location.index,
+					title: 'Download to ' + location.name,
+					contexts: ['link']
+				})
+			})
+		}
+	})
+}
+
+createContextMenu()
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId === 'transmitter-add') {
 		return addUrl(info.linkUrl)
+	} else if (info.menuItemId.startsWith('transmitter-add-loc-')) {
+		let index = parseInt(info.menuItemId.substr('transmitter-add-loc-'.length))
+		browser.storage.local.get('server').then(({server}) => {
+			let path = server.locations[index].path
+			addUrl(info.linkUrl, path)
+		});
 	}
 })
 
@@ -165,9 +198,15 @@ browser.storage.onChanged.addListener((changes, area) => {
 	const oldv = changes.server.oldValue
 	const newv = changes.server.newValue
 	if (!oldv || oldv.base_url !== newv.base_url || oldv.username !== newv.username ||
-		oldv.password !== newv.password || oldv.badge_interval !== newv.badge_interval || oldv.badge !== newv.badge) {
+		oldv.password !== newv.password || oldv.badge_interval !== newv.badge_interval || oldv.badge !== newv.badge ||
+		arraysEqualDeep(oldv.locations, newv.locations)) {
 		setupExtractor()
 		setupBadge()
 		updateBadge()
+		createContextMenu()
 	}
 })
+
+function arraysEqualDeep (arr1, arr2) {
+	return JSON.stringify(arr1) !== JSON.stringify(arr2)
+}
--- options.js
+++ options.js
@@ -1,6 +1,11 @@
 const form = document.getElementById('options-form')
 const warning = document.getElementById('warning')
-const btn = form.querySelector('button')
+const saveBtn = form.querySelector('#save')
+const locations = form.querySelector('#locations')
+const location_name = form.querySelector('#location_name')
+const location_path = form.querySelector('#location_path')
+const location_new = form.querySelector('#location_new')
+const location_delete = form.querySelector('#location_delete')
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
@@ -9,6 +14,7 @@ form.addEventListener('submit', e => {
 		return obj
 	}, {})
 	delete data['']
+	data.locations = saveLocations()
 	/*
 	When this works everywhere, replace <all_urls> with http://transmitter.web-extension/* in manifest.json.
 	Unfortunately, right now this is unsupported in Firefox and Edge…
@@ -28,9 +34,9 @@ form.addEventListener('submit', e => {
 		warning.hidden = true
 	}
 	browser.storage.local.set({server: data}).then(() => {
-		btn.innerHTML = 'Saved!'
+		saveBtn.innerHTML = 'Saved!'
 		setTimeout(() => {
-			btn.innerHTML = 'Save'
+			saveBtn.innerHTML = 'Save'
 		}, 600)
 	})
 })
@@ -43,6 +49,78 @@ browser.storage.local.get('server').then(({server}) => {
 		const el = form.querySelector('[name="' + x + '"]')
 		if (el) {
 			el.value = server[x]
+		} else if (x === 'locations') {
+			renderLocations(server[x])
 		}
 	}
 })
+
+function renderLocations (locationsData) {
+	locations.querySelectorAll('option:not(:first-child)').forEach(opt => opt.remove())
+	locationsData.map(loc => createLocationOption(loc.name, loc.path))
+		.forEach(opt => locations.appendChild(opt))
+}
+
+locations.addEventListener('change', refreshLocationEditor);
+
+function refreshLocationEditor () {
+	if (locations.selectedIndex == -1) {
+		location_name.value = ''
+		location_path.value = ''
+		location_name.disabled = true
+		location_path.disabled = true
+		location_delete.disabled = true
+	} else {
+		let option = locations.options[locations.selectedIndex]
+		location_name.value = option.dataset.name
+		location_path.value = option.dataset.path
+		location_name.disabled = false
+		location_path.disabled = false
+		location_delete.disabled = false
+	}
+}
+
+location_name.addEventListener('input', () => {
+	let option = locations.options[locations.selectedIndex]
+	option.dataset.name = location_name.value
+	refreshLocationOptionText(option)
+});
+
+location_path.addEventListener('input', () => {
+	let option = locations.options[locations.selectedIndex]
+	option.dataset.path = location_path.value
+	refreshLocationOptionText(option)
+});
+
+location_delete.addEventListener('click', () => {
+	locations.options[locations.selectedIndex].remove()
+	if (locations.options.length > 1) {
+		locations.selectedIndex = locations.options.length - 1
+	}
+	refreshLocationEditor();
+});
+
+location_new.addEventListener('click', () => {
+	let option = createLocationOption('New location', '/path/to/location')
+	locations.appendChild(option)
+	locations.selectedIndex = locations.options.length - 1
+	refreshLocationEditor()
+	location_name.select()
+});
+
+function createLocationOption (name, path) {
+	let option = document.createElement('option')
+	option.dataset.name = name
+	option.dataset.path = path
+	refreshLocationOptionText(option)
+	return option
+}
+
+function refreshLocationOptionText (option) {
+	option.innerText = option.dataset.name + ' [' + option.dataset.path + ']'
+}
+
+function saveLocations () {
+	return [...locations.querySelectorAll('option:not(:first-child)')]
+		.map((opt, index) => ({ name: opt.dataset.name, path: opt.dataset.path, index: index }))
+}
